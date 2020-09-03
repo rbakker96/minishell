8c6d041fb4558248aa170073f8617859fa1a7c3f
Qing Li <qli@pop-os.localdomain>
2020-09-03 14:26:59 +0200

    test branch - main.c

--- main.c
+++ main.c
@@ -6,13 +6,15 @@
 /*   By: roybakker <roybakker@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/31 10:42:36 by roybakker     #+#    #+#                 */
-/*   Updated: 2020/09/03 12:00:34 by roybakker     ########   odam.nl         */
+/*   Updated: 2020/09/03 14:26:34 by qli           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "structs.h"
 #include "minishell.h"
 #include <stdio.h>
+
+// changes made by qli
 
 int		main(void)
 {
