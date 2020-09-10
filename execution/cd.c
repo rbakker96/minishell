b76ea6c3abb365057c4cd6351f80102c35987abe
Qing Li <qli@pop-os.localdomain>
2020-09-10 11:44:29 +0200

    qli - add cd

--- execution/cd.c
+++ execution/cd.c
@@ -6,15 +6,21 @@
 /*   By: roybakker <roybakker@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/09 14:50:20 by roybakker     #+#    #+#                 */
-/*   Updated: 2020/09/09 16:00:54 by roybakker     ########   odam.nl         */
+/*   Updated: 2020/09/10 11:44:07 by qli           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+// void	go_to_home(t_data *data, int command, int *token)
+// {
+	
+// }
+
 void	execute_cd(t_data *data, int command, int *token)
 {
-	if (data && command && (*token))
-		print(".");
+	chdir(data->commands[command]->tokens[(*token) + 1]);
+//		CLEAR STRUCT WHEN FAIL
+//
 	return ;
 }
