11a11353bc93afd49dbbe256299622f12318d45d
Qing Li <qli@f1r6s15.codam.nl>
2020-10-15 17:00:53 +0200

    qli - agreed plan on finalising piping

--- 5_execution/execute_loop.c
+++ 5_execution/execute_loop.c
@@ -6,7 +6,7 @@
 /*   By: rbakker <rbakker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/12 16:36:24 by rbakker       #+#    #+#                 */
-/*   Updated: 2020/10/13 16:44:52 by rbakker       ########   odam.nl         */
+/*   Updated: 2020/10/15 16:59:33 by qli           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,22 +49,26 @@ void		execution_loop(t_data *data, int cmd, int tkn)
 	{
 		initialise_struct_elements(data);
 		shell_expansions(data, cmd, 0);
-		//create_pipe_fd(data, cmd);
-		initialize_pipes(data, cmd);
+		//create_pipe_fd(data, cmd); - ROY
+		// initialize_pipes(data, cmd); - ROY
 		tkn = 0;
 		while (tkn < data->commands[cmd]->token_amount)
 		{
-			if (redirections(data, cmd, tkn) != -1)
-			{
-				update_token_list(data, cmd, tkn, 0);
-				tkn = 0;
-				value = data->commands[cmd]->tokens[tkn];
-				if (value != NULL)
-					execute_command(data, cmd, &tkn, value);
-				data->pipe_pos++;
-			}
-			else
-				break ;
+			//set iostream to replace -> redirections(data, cmd, tkn) != -1 - ROY
+			// update_token_list(data, cmd, tkn, 0); //remove > < tokens, do not remove non-existing tokens - ROY
+			// data->iostream[READ] / data->iostream[WRITE]
+			tkn = 0;
+			value = data->commands[cmd]->tokens[tkn];
+			// QING process below
+			//fork()
+			// child process
+				// close_not_used_fds()
+				// execute_command(data, cmd, &tkn, value);
+				// exit();
+			// parent process
+				// close_used_fds()
+				// data->pipe_pos++;
+				// wait(&status);
 		}
 		cmd++;
 	}
