390f41bd6959b84efe358854055ed569b2f758eb
Qing Li <qli@pop-os.localdomain>
2020-10-26 17:30:58 +0100

    removed printf

--- 7_input_validation/input_validation.c
+++ 7_input_validation/input_validation.c
@@ -6,7 +6,7 @@
 /*   By: rbakker <rbakker@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/07 13:52:27 by rbakker       #+#    #+#                 */
-/*   Updated: 2020/10/26 17:01:26 by qli           ########   odam.nl         */
+/*   Updated: 2020/10/26 17:30:41 by qli           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ int		input_validation(t_data *data)
 		validate_double_quotes(data, &character) == -1 ||
 		validate_end_of_line_command(data, &character, 0) == -1)
 	{
-		printf("1\n");
 		validation_error(data, character, 1);
 		return (-1);
 	}
@@ -32,7 +31,6 @@ int		input_validation(t_data *data)
 		validate_start_of_line_command(data, &character) == -258 ||
 		validate_end_of_line_command(data, &character, 0) == -258)
 	{
-		printf("2\n");
 		validation_error(data, character, 258);
 		return (-1);
 	}
